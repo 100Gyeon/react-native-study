23852da7282f09019a765a4a997bfce43da7de97
100Gyeon <bjy9966@naver.com>
2022-02-02 00:39:06 +0900

    ✨feat: 탭 내비게이션 라이브러리 추가 및 사용

---
+++ src/pages/Delivery.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Ing from './Ing';
+import Complete from './Complete';
+
+const Stack = createNativeStackNavigator();
+
+function Delivery() {
+  return (
+    <Stack.Navigator>
+      {/* Stack처럼 지도 위에 완료 화면을 쌓기 위함 */}
+      <Stack.Screen name="Ing" component={Ing} options={{headerShown: false}} />
+      <Stack.Screen
+        name="Complete"
+        component={Complete}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default Delivery;
